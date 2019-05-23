9afdb2865ef0b447e2ce2faa46a92c31f7a678b0
Mbiloniya <mahesh@darkbears.com>
2019-05-23 12:02:01 +0530

    semi colon added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-      System.out.println("To check Poll Scm work")
+      System.out.println("To check Poll Scm work");
    }
 }
